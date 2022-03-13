618a53ff4b8a91e8fc682b1ad25a354ac7ffe41a
Atanu Garai <{25636407}+{atanu-sw}@users.noreply.github.com>
2022-03-13 23:02:02 +0530

    added linting

---
+++ src/components/MobileNumber/MobileNumber.stories.js
@@ -0,0 +1,10 @@
+import { MobileNumber } from "./MobileNumber";
+
+export default {
+  title: "Mobile Number",
+  component: MobileNumber,
+};
+
+const Template = (args) => <MobileNumber />;
+
+export const Default = Template.bind({});
