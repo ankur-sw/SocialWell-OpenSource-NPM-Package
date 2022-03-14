7da420b7f75f3cb2b2b85001007f44fa134f070f
Codatrix <ankur.codatrix@gmail.com>
2022-03-14 21:26:35 +0530

    Input component files modified

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,12 +1,54 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "./input.css";
 
 /**
  * Primary UI component for user interaction
  */
-export const Input = (props) => {
-  const { size = "medium", ...rest } = props;
+// export const Input = (props) => {
+//   const { size = "medium", ...rest } = props;
 
-  return <input className={`input ${size}`} {...rest} />;
+//   return <input className={`input ${size}`} {...rest} />;
+// };
+
+// Helper
+const handleChange =
+  ({ onChange }) =>
+  () => {
+    onChange();
+  };
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Input = ({ type, value, placeholder, onChange, isDisabled }) => {
+  return (
+    <input
+      className={`input`}
+      type={type}
+      value={value}
+      placeholder={placeholder}
+      onChange={handleChange({ onChange })}
+      isDisabled={isDisabled}
+    />
+  );
+};
+
+// prop types
+Input.propTypes = {
+  value: PropTypes.string,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  isDisabled: PropTypes.bool,
+};
+
+// default props
+Input.defaultProps = {
+  value: "",
+  type: "text",
+  placeholder: "",
+  onChange: () => {},
+  isDisabled: false,
 };
--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -2,15 +2,34 @@ import React from "react";
 
 import { Input } from "./Input";
 
+// // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// export default {
+//   title: "Input",
+//   component: Input,
+// };
+
+// //----------
+// // Variants
+// //----------
+// export const Small = () => <Input size="small" placeholder="Small size" />;
+// export const Medium = () => <Input size="medium" placeholder="Medium size" />;
+// export const Large = () => <Input size="large" placeholder="Large size" />;
+
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Input",
   component: Input,
+  args: {
+    placeholder: "Write something...",
+  },
 };
 
-//----------
-// Variants
-//----------
-export const Small = () => <Input size="small" placeholder="Small size" />;
-export const Medium = () => <Input size="medium" placeholder="Medium size" />;
-export const Large = () => <Input size="large" placeholder="Large size" />;
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+
+export const Value = Template.bind({});
+Value.args = {
+  value: "Lorem ipsum dolor, sit amet.",
+};
