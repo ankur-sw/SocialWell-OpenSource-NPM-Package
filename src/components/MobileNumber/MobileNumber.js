a14b840c911849310f08ca5cf0cf66344db50675
Atanu Garai <{25636407}+{atanu-sw}@users.noreply.github.com>
2022-03-15 08:16:32 +0530

    styled component

--- src/components/MobileNumber/MobileNumber.js
+++ src/components/MobileNumber/MobileNumber.js
@@ -1,5 +1,4 @@
-import { InputContainer } from "./styled";
-import styled from "styled-components";
+import { Container, TextInput, InputContainer, Icon } from "styled-components";
 
 export const MobileNumber = ({
   country = "India",
@@ -9,27 +8,6 @@ export const MobileNumber = ({
   placeholder = "Enter your mobile number",
   validators,
 }) => {
-  const Container = styled.div`
-    font-family: "Arial";
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5px;
-    border: 1px solid;
-    border-radius: 5px;
-    &:hover {
-      box-shadow: 1px 1px 1px thistle;
-    }
-  `;
-
-  const InputContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    flex: 1 0;
-    align-items: center;
-    min-width: 0;
-  `;
-
   return (
     <Container>
       <InputContainer>
